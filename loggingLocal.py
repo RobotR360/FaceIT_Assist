63b1867904b39ff9116d9824b2098cd92f644f4c
RobotR360 <156365663+RobotR360@users.noreply.github.com>
2025-09-16 14:31:24 +0600

    Update loggingLocal.py

--- loggingLocal.py
+++ loggingLocal.py
@@ -1,9 +1,10 @@
 import os
 from datetime import datetime
 
-
+#Ведение логов с указанием времени
 def log_print(text):
 	os.makedirs(f"src\\logs\\", exist_ok=True)
 	now = datetime.now()
 	with open("log.txt", "a") as file:
+
 		file.write(now.strftime("%H:%M %d.%m.%Y")+f"\t\t{text}\n")
